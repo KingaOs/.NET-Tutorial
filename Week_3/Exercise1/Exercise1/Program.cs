a48330a5c1c58a6c512a3d50e0855e88f768e7af
kosinska <kosinska@milisystem.pl>
2020-12-07 08:10:23 +0100

    Week 3 - Exercise 1-2

---
+++ Week_3/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int year;
+            int month;
+            int day;
+            Console.WriteLine("Podaj imię:");
+            var name = Console.ReadLine();
+            Console.WriteLine("Podaj swoją datę urodzenia. \nPodaj rok urodzenia:");
+            if (!int.TryParse(Console.ReadLine(),out year))
+                throw new Exception("Podałeś złe dane!");
+            Console.WriteLine("Podaj miesiąc:");
+            if (!int.TryParse(Console.ReadLine(), out month))
+                throw new Exception("Podałeś złe dane!");
+            Console.WriteLine("Podaj dzień:");
+            if (!int.TryParse(Console.ReadLine(), out day))
+                throw new Exception("Podałeś złe dane!");
+            Console.WriteLine("Podaj miejsce urodzenia:");
+            var placeOfBirth = Console.ReadLine();
+
+            var age = CheckUserAge(year,month,day);
+
+            Console.WriteLine($"Cześć {name}. Urodziłeś się w {placeOfBirth} i masz {age} lat.");
+            Console.ReadLine();
+
+        }
+
+        static int CheckUserAge(int year, int month, int day)
+        {
+            var currentDate = DateTime.Now;
+            var currentYear = DateTime.Now.Year;
+            int age = currentYear - year;
+            var currentDayOfYear = DateTime.Now.DayOfYear;
+            var birthDayOfYear = new DateTime(currentYear, month, day).DayOfYear;
+            if (currentDayOfYear < birthDayOfYear)
+                age--;
+            return age;
+        }
+
+
+    }
+}
---
+++ Week_3/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfGuesses = 0;
+            Random rand = new Random();
+            var randomNumber = rand.Next(0, 101);
+            Console.WriteLine("Wylosowano liczbę z przedziału 0-100. Zgadnij jaka to liczba!");
+            int userInput;
+            do
+            {
+                numberOfGuesses++;
+                userInput = int.Parse(Console.ReadLine());
+                if (userInput > randomNumber)
+                    Console.WriteLine("Niżej");
+                if (userInput < randomNumber)
+                    Console.WriteLine("Wyżej");
+            } while (userInput != randomNumber);
+
+            Console.WriteLine($"Dobrze! Odgadłeś liczbę w {numberOfGuesses} próbach!");
+        }
+    }
+}
